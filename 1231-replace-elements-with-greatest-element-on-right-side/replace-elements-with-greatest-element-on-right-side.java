52b0d7fcad747201714a6315bbb39ea5dddb9d33
Ujjwal Kumar <97801461+ujjwalkur@users.noreply.github.com>
2024-07-25 19:19:46 +0530

    Time: 2 ms (94.95%) | Memory: 46.7 MB (18.24%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max = -1;
+        for(int i = arr.length-1; i>= 0; i--){
+            int temp = arr[i];
+            arr[i] = max;
+            if(temp > max){
+                max = temp;
+            }
+        }
+        return arr;
+    }
+}
+
+// class Solution {
+//     public int[] replaceElements(int[] arr) {
+//         int max = -1;
+//         for (int i = arr.length - 1; i >= 0; i--) {
+//             int tmp = arr[i];
+//             arr[i] = max;
+//             if (tmp > max) {
+//                 max = tmp;
+//             }
+//         }
+//         return arr;
+//     }
+// }
