035684297e3ce7807376d09acc386cbfda66cd77
Ujjwal Kumar <97801461+ujjwalkur@users.noreply.github.com>
2024-08-23 19:49:33 +0530

    Time: 13 ms (40.79%) | Memory: 45.1 MB (22.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Remove non-alphanumeric characters and convert to lowercase
+        
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         
-        // Two pointers approach
-        int i = 0, j = s.length() - 1;
-        while (i < j) {
-            // If characters are not equal, it's not a palindrome
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
+        int i;
+        int j;
+        
+        for(i = 0, j = s.length()-1; i < j; i++, j--){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+         
+            if(left != right){
+                break;
             }
-            // Move pointers towards the center
-            i++;
-            j--;
+            
         }
-        // If pointers meet in the middle, it's a palindrome
+        if(i < j){
+            return false;
+        }
         return true;
     }
 }
