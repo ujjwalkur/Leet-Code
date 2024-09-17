d3bec8b3bd43341ca2303b875df89426c819f078
Ujjwal Kumar <97801461+ujjwalkur@users.noreply.github.com>
2024-09-17 12:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (55.78%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -6,23 +6,31 @@ class Solution {
             return nums[0];
         }
         
-        for (int i = 0; i < n; i++) {
-    
-            if (i == 0) {
-                if (nums[i] != nums[i + 1]) {
-                    return nums[i];
-                }
-            } else if (i == n - 1) {
-                if (nums[i] != nums[i - 1]) {
-                    return nums[i];
-                }
-            } else {
-                if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
-                    return nums[i];
-                }
-            }
+        if(nums[0] != nums[1]){
+            return nums[0];
         }
         
-        throw new IllegalArgumentException("Input array is invalid.");
+        if(nums[n-1] != nums[n-2]){
+            return nums[n-1];
+        }
+        
+        int start = 1;
+        int end = n - 2;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]){
+                return nums[mid];
+            }
+            
+            if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 }
