1678fb8b38826bb5da08eefb2eb47158b373c2a4
Ujjwal Kumar <97801461+ujjwalkur@users.noreply.github.com>
2024-10-01 00:04:05 +0530

    Time: 12 ms (97.27%) | Memory: 46.4 MB (62.62%) - LeetSync

---
+++ 561-array-partition/array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int total = 0;
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length; i=i+2){
+            total+=nums[i];
+        }
+        return total;
+    }
+}
